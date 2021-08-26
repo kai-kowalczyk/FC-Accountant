9b932e0b891c4b5081b06b1e025f81fee438efbe
Kai Kowalczyk <kai.kowalczyk@gmail.com>
2021-08-26 12:43:32 +0200

    defining actions in program

--- accountant.py
+++ accountant.py
@@ -1,5 +1,19 @@
 import sys
 
+action = input('Jaką akcję chcesz wykonać? (saldo/zakup/sprzedaż/stop)')
+if action == 'saldo':
+    print('saldo')
+elif action == 'zakup':
+    print('zakup')
+elif action == 'sprzedaż':
+    print('sprzedaż')
+elif action == 'stop':
+    print('stop')
+else:
+    print('Błąd: wybierz jedną z wymienionych akcji.')
+    quit()
+
+
 logs = []
 if sys.argv[1] == 'saldo':
     logs.append(f'Zmiana saldo o: {sys.argv[2]} gr, komentarz: {sys.argv[3]}')
@@ -9,3 +23,5 @@ elif sys.argv[1] == 'sprzedaż':
 
 elif sys.argv[1] == 'zakup':
     logs.append(f'Zakupiono {sys.argv[4]} szt. produktu {sys.argv[2]} o wartości {sys.argv[5]}gr/szt.')
+
+print(logs)
