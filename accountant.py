061dcc2cf26e8323b1f5f4993e6e4ee269a8cfa7
Kai Kowalczyk <kai.kowalczyk@gmail.com>
2021-08-27 12:38:13 +0200

    continuing working on action loop, added new starting modes

--- accountant.py
+++ accountant.py
@@ -1,36 +1,55 @@
 import sys
 
-logs = []
+logs = [] #historia operacji
+
+account_balance = 2000.0 #Saldo poczatkowe
+
+store = {
+    'mleko': {'quantity': 20, 'price': 4.50},
+    'parasol': {'quantity': 5, 'price': 25.00}
+} #Magazyn sklepu
 
 while True:
-    action = input('Jaką akcję chcesz wykonać? (saldo/zakup/sprzedaż/stop)')
-    if action == 'saldo':
-        balance_change = int(input('Wprowadź kwotę o jaką zmienia się stan konta (w groszach). W przypadku wypłaty z konta poprzedź kwotę znakiem minus ("-"): '))
+    action = input('Jaką akcję chcesz wykonać? (saldo/zakup/sprzedaz/stop)')
+    if action == 'stop':
+        print('Koniec działania programu!')
+        break #przejscie do podsumowania
+    elif action == 'saldo':
+        balance_change = float(input('Wprowadź kwotę o jaką zmienia się stan konta. W przypadku wypłaty z konta poprzedź kwotę znakiem minus ("-"): '))
         comment = input('Komentarz do zmiany salda: ')
-        logs.append(f'Saldo konta zmienione o {balance_change}, komentarz: {comment}')
-        #print('saldo')
+        if (balance_change < 0) and (account_balance + balance_change < 0):
+            print('Za mało środków na koncie!')
+        logs.append(f'Zmiana saldo o: {balance_change} zł, komentarz: {comment}')
+        account_balance = account_balance + balance_change
     elif action == 'zakup':
         product_id = input('Wprowadź nazwę produktu: ')
-        price = int(input('Wprowadź cenę jednostkową produktu (w groszach): '))
+        price = float(input('Wprowadź cenę jednostkową produktu: '))
         quantity = int(input('Wprowadź liczbę sztuk zakupionego produktu: '))
-        logs.append(f'Zakupiono {quantity} szt. produktu {product_id} o wartości {price}gr/szt.')
-        #print('zakup')
-    elif action == 'sprzedaż':
-        print('sprzedaż')
-    elif action == 'stop':
-        print('stop')
-        break #przejscie do podsumowania
+        logs.append(f'Zakupiono {quantity} szt. produktu {product_id} o wartości {price} zł/szt.')
+    elif action == 'sprzedaz':
+        product_id = input('Wprowadź nazwę produktu: ')
+        price = int(input('Wprowadź cenę jednostkową produktu (w groszach): '))
+        quantity = int(input('Wprowadź liczbę sztuk sprzedanego produktu: '))
+        logs.append(f'Sprzedano {quantity} szt. produktu {product_id} o wartości {price}gr/szt.')
     else:
         print('Błąd: wybierz jedną z wymienionych akcji.')
+        continue
 
 
 
 if sys.argv[1] == 'saldo':
-    logs.append(f'Zmiana saldo o: {sys.argv[2]} gr, komentarz: {sys.argv[3]}')
+    logs.append(f'Zmiana saldo o: {sys.argv[2]} zł, komentarz: {sys.argv[3]}')
 
-elif sys.argv[1] == 'sprzedaż':
-    logs.append(f'Sprzedano {sys.argv[4]} szt. produktu {sys.argv[2]} o wartości {sys.argv[3]}gr/szt.')
+elif sys.argv[1] == 'sprzedaz':
+    logs.append(f'Sprzedano {sys.argv[4]} szt. produktu {sys.argv[2]} o wartości {sys.argv[3]} zł/szt.')
 
 elif sys.argv[1] == 'zakup':
-    logs.append(f'Zakupiono {sys.argv[4]} szt. produktu {sys.argv[2]} o wartości {sys.argv[5]}gr/szt.')
+    logs.append(f'Zakupiono {sys.argv[4]} szt. produktu {sys.argv[2]} o wartości {sys.argv[5]} zł/szt.')
+
+elif sys.argv[1] == 'konto':
+    print(f'Stan konta: {account_balance}')
+
+elif sys.argv[1] == 'magazyn':
+    print(store)
+
 print(logs)
